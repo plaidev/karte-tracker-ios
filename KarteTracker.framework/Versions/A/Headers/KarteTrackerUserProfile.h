5337f16bac216ee2a584ad5012597211119ecbb9
takaaki7 <nakama67006700@gmail.com>
2018-03-26 08:17:58 +0900

    1.0.1

---
+++ KarteTracker.framework/Versions/A/Headers/KarteTrackerUserProfile.h
@@ -0,0 +1,18 @@
+//
+//  KarteTrackerUserProfile.h
+//  Pods
+//
+
+#ifndef KarteTrackerUserProfile_h
+#define KarteTrackerUserProfile_h
+
+#import <Foundation/Foundation.h>
+
+@interface KarteTrackerUserProfile : NSObject
+
+- (id)init;
+- (NSString*) getVisitorId;
+
+@end
+
+#endif /* KarteTrackerUserProfile_h */
