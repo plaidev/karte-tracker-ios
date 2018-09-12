b9a976f521fb48b5aa9a0500c4cf5a7eecd1ad97
tomoponzoo <tomoki.koga@plaid.co.jp>
2018-09-12 21:11:54 +0900

    Update example app

--- Example/NotificationService/NotificationService.m
+++ Example/NotificationService/NotificationService.m
@@ -6,4 +6,13 @@
 
 @implementation NotificationService
 
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    
+    if ([self canHandleRemoteNotification:request.content.userInfo]) {
+        [super didReceiveNotificationRequest:request withContentHandler:contentHandler];
+    } else {
+        contentHandler(request.content);
+    }
+}
+
 @end
