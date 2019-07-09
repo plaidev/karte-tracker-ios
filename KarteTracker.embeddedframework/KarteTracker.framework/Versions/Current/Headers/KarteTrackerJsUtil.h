9d5a9847f6df43307b1df3c39897e0a2ae9ef4eb
plaid-inc <dev.share@plaid.co.jp>
2019-07-09 00:39:54 -0700

    1.6.2

--- KarteTracker.embeddedframework/KarteTracker.framework/Versions/Current/Headers/KarteTrackerJsUtil.h
+++ KarteTracker.embeddedframework/KarteTracker.framework/Versions/Current/Headers/KarteTrackerJsUtil.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <WebKit/WebKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -14,6 +15,9 @@ NS_ASSUME_NONNULL_BEGIN
 + (NSString *)stringByAppendingUserSyncQueryParameter:(NSString *)appKey withURLString:(NSString *)url NS_SWIFT_NAME(stringByAppendingUserSyncQueryParameter(appKey:url:));
 + (NSURL *)URLByAppendingUserSyncQueryParameter:(NSString *)appKey withURL:(NSURL *)url NS_SWIFT_NAME(URLByAppendingUserSyncQueryParameter(appKey:url:));
 + (nullable NSString *)userSyncQueryParameter:(NSString *)appKey;
+
++ (void)setUserSyncScriptWithWebView:(WKWebView *)webView NS_SWIFT_NAME(setUserSyncScript(with:));
++ (void)setUserSyncScriptWithWebView:(WKWebView *)webView appKey:(NSString *)appKey NS_SWIFT_NAME(setUserSyncScript(with:appKey:));
 @end
 
 NS_ASSUME_NONNULL_END
